9684c56caaba4adba392e179af1f74819b6db0b3
Uma Shankar Yadav <akgshankar@gmail.com>
2018-01-24 19:32:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var trainingApp = angular.module('trainingApp',['ngMessages']);
 
-trainingApp.controller('playerController',function($scope){
+trainingApp.controller('playersController',function($scope){
+    
     var playerTemplate = {
         name:null,
         nickname:null,
@@ -8,14 +9,48 @@ trainingApp.controller('playerController',function($scope){
         team:null
     } 
 
-    $scope.player={};
+    $scope.newPlayer={};
 
-    $scope.submitForm = function(isValid) {
-        // check to make sure the form is completely valid
-        if (isValid) {
-          console.log('we are good to go');
+    $scope.players=[
+        {name:'Sachin Ramesh Tendulkar',nickname:'Master',email:'isachin@gmail.com',team:'India'},
+        {name:'Viraat Kohli',nickname:'Kohli',email:'kohli@gmail.com',team:'India'},
+        {name:'Harbhajan',nickname:'BHajji',email:'bhajji@gmail.com',team:'India'},
+        {name:'Bhuvneshwar Kumar',nickname:'Bhuvi',email:'bhuvi@gmail.com',team:'India'},
+        {name:'Inzemam ul Haq',nickname:'Aloo',email:'inzm@gmail.com',team:'Pakistaan'}
+    ];
+
+    function addPlayer(player){
+        $scope.players.push(player);
+    }
+
+    $scope.deletePlayer= function(player){
+        let indx= $scope.players.indexOf(player);
+        if(indx !=-1){
+            $scope.palyers.splice(player);
         }
-      };
+    }
+
+    $scope.submitForm = function(isFormValid,player) {
+        // check to make sure the form is completely valid
+        if(isFormValid){
+            if (isPlayerInfoValid(player)) {
+                console.log('we are good to go');
+                addPlayer(player);
+              }
+        }
+        
+    };
+
+    $scope.search = function(inputVal){
+    }
+
+    $scope.add = function(player){
+    }
+
+    function isPlayerInfoValid(player){
+        return true;
+    }
+
 });
 
 trainingApp.controller('teamsController',function($scope){
@@ -24,4 +59,34 @@ trainingApp.controller('teamsController',function($scope){
 
 trainingApp.controller('tournamentController',function($scope){
      
+});
+
+
+app.service('playersService', function () {
+    this.getPlayers = function () {
+        return players;
+    };
+
+    this.insertPlayer = function (player) {
+        var topID = players.length + 1;
+        player.id = topID;
+        players.push(players);
+    };
+
+    this.deletePlayer = function (player) {
+        
+    };
+
+    this.getCustomer = function (id) {         
+        
+    };
+
+    var players = [
+        {name:'Sachin Ramesh Tendulkar',nickname:'Master',email:'isachin@gmail.com',team:'India'},
+        {name:'Viraat Kohli',nickname:'Kohli',email:'kohli@gmail.com',team:'India'},
+        {name:'Harbhajan',nickname:'BHajji',email:'bhajji@gmail.com',team:'India'},
+        {name:'Bhuvneshwar Kumar',nickname:'Bhuvi',email:'bhuvi@gmail.com',team:'India'},
+        {name:'Inzemam ul Haq',nickname:'Aloo',email:'inzm@gmail.com',team:'Pakistaan'}
+    ];
+
 });
